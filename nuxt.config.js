a1cb2445242173e22725b78be7703ad2abab4558
jakobberglund <contact@jakobberglund.com>
2021-10-10 13:31:53 +0200

    update meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,12 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Epilogen är ett band från Stockholm som spelar suggestiv, avskalad, industriell rock på svenska.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
