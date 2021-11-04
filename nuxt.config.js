96bc95c9cb762095ae8135bd2a2bfe64094812de
jakobberglund <contact@jakobberglund.com>
2021-11-04 09:57:51 +0100

    revert to older commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin')
-
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -58,7 +56,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['@/assets/styles/reset.css'],
+  css: ['@/assets/styles/reset.css', '@/assets/styles/index.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -72,14 +70,11 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
-    '@nuxt/image',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    plugins: [new VanillaExtractPlugin()],
-  },
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,31 @@
 <template>
-  <div :class="indexClass">
+  <div class="wrapper">
     <div>
-      <h1 :class="indexH1Class">Epilogen, kommer snart.</h1>
+      <h1>Epilogen, kommer snart.</h1>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { indexClass, indexH1 } from './index.css'
 
-export default Vue.extend({
-  computed: {
-    indexClass: () => indexClass,
-    indexH1Class: () => indexH1,
-  },
-})
+export default Vue.extend({})
 </script>
+
+<style scoped>
+.wrapper {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 2rem;
+  text-align: center;
+}
+
+h1 {
+  max-width: 35ch;
+  font-size: clamp(1rem, calc(1rem + 2vw), 1.25rem);
+  font-weight: 200;
+}
+</style>
